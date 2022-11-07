9e56b71a0464db8877fa221d559e921ac5123f50
Keks <keks@htmlacademy.ru>
2022-11-07 16:20:19 +0300

    :heavy_check_mark: Сборка #6

--- 6/scripts/modal-window.js
+++ 6/scripts/modal-window.js
@@ -12,4 +12,20 @@ for (const modalWindow of modalWindows) {
   for (const closeButton of closeButtons) {
     closeButton.addEventListener('click', () => modalWindow.classList.remove('visible'));
   }
+
+  let overlays = modalWindow.getElementsByClassName('overlay');
+  for (const overlay of overlays) {
+    overlay.addEventListener('click', () => modalWindow.classList.remove('visible'));
+  }
+
+  let input_groups = modalWindow.getElementsByClassName('input-group');
+  for (const input_group of input_groups) {
+    let input = input_group.querySelector('input');
+
+    let minusButton = input_group.querySelector('.minus');
+    minusButton.addEventListener('click', () => input.stepDown());
+
+    let plusButton = input_group.querySelector('.plus');
+    plusButton.addEventListener('click', () => input.stepUp());
+  }
 }
